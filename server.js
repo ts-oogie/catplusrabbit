5677e7f388220fb206e81839da67e72d9960dc9f
MyMac <onagususa@gmail.com>
2023-02-01 00:40:14 -0500

    Changed

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();      
 
 const port = process.env.PORT || 8000;    
+
+app.use('/', express.static(__dirname + '/dist'));
  
 app.get('/' , function(req, res ){ 
     res.sendFile(__dirname + '/dist/index.html');
